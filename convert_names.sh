b6e1ea4b6598c7f039feea18b550cc7dbd30f024
Kevin-Zhang-TW <59301312+Kevin-Zhang-TW@users.noreply.github.com>
2019-12-30 23:00:51 +0800

    Update convert_names.sh

--- convert_names.sh
+++ convert_names.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
-path=~/Downloads/test_data/
+working_dir=~/Downloads
+path=$working_dir/test_data
+read dir
+if [ $dir != "test_data" ]
+then
+	rm test_data/*
+	unzip ${dir}.zip -d $working_dir/test_data
+fi
 for i in $path/I.*
 do
- 	mv $i ${path}"${i##*.}".in
+ 	mv $i ${path}/"${i##*.}".in
 done
 for i in $path/O.*
 do
-	mv $i ${path}"${i##*.}".out
+	mv $i ${path}/"${i##*.}".out
 done
+rm $dir.zip
 echo transform complete ~ 
