1963d93a5ace83c965462be26a2da5fdc39c9145
Kevin-Zhang-TW <59301312+Kevin-Zhang-TW@users.noreply.github.com>
2019-12-30 23:03:47 +0800

    Update convert_names.sh

--- convert_names.sh
+++ convert_names.sh
@@ -4,8 +4,8 @@ path=$working_dir/test_data
 read dir
 if [ $dir != "test_data" ]
 then
-	rm test_data/*
-	unzip ${dir}.zip -d $working_dir/test_data
+	rm $working_dir/test_data/*
+	unzip $working_dir/${dir}.zip -d $working_dir/test_data
 fi
 for i in $path/I.*
 do
@@ -15,5 +15,5 @@ for i in $path/O.*
 do
 	mv $i ${path}/"${i##*.}".out
 done
-rm $dir.zip
+rm $working_dir/$dir.zip
 echo transform complete ~ 
