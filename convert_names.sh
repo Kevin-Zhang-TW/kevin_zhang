b4d1511430db94db4125b271ca1e39a4bfd30988
Kevin-Zhang-TW <59301312+Kevin-Zhang-TW@users.noreply.github.com>
2019-12-31 11:43:19 +0800

    Update convert_names.sh

--- convert_names.sh
+++ convert_names.sh
@@ -2,25 +2,31 @@
 working_dir=~/Downloads
 target=~/test_data
 test_zip=${1:-""}
-if [ $test_zip == "" ]
+back_up_dir=~/old_test_data
+if [[ $test_zip == "" ]]
 then
 	read test_zip
 fi
-
-if [ $test_zip != "test_data" ]
+if [ ! -e $back_up_dir/$test_zip ]
 then
 	rm $working_dir/test_data/*
 	unzip $working_dir/${test_zip}.zip -d $target
+	unzip $working_dir/${test_zip}.zip -d $back_up_dir/$test_zip
+else 
+	cp $back_up_dir/${test_zip}/* $target/
 fi
 ## transform formats
 for i in $target/I.*
 do
  	mv $i ${target}/"${i##*.}".in
 done
-for i in $target/O.*
+for i in $target/O.* 
 do
 	mv $i ${target}/"${i##*.}".out
 done
 ## delete zip file
-rm $working_dir/$test_zip.zip
+if [ -e $working_dir/$test_zip ]
+then
+	rm $working_dir/$test_zip.zip
+fi
 echo transform complete ~ 
