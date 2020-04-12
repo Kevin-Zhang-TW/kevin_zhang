8ac91eb94077c94159d608e7f1a4e1179a5c8f24
Kevin-Zhang-TW <59301312+Kevin-Zhang-TW@users.noreply.github.com>
2020-04-12 12:16:45 +0800

    Create 讀檔小撇步.cpp

---
+++ 讀檔小撇步.cpp
@@ -0,0 +1,10 @@
+//  Way 1
+freopen("file", "w", stdin);
+freopen("file", "r", stdout);
+//  Way 2
+ifstream in("file");
+ofstream out("file");
+int a, b;
+in >> a >> b;
+out << a + b << '\n';
+// Magic \O/ 
